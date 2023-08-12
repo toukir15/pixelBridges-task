c67d4adf3f0d4969faf7056998bb95285153a048
Tanvir <toukir.developer.bd@gmail.com>
2023-08-12 16:23:00 +0600

    Added Section: added toconomics section

--- src/components/Home/Tokenomics.jsx
+++ src/components/Home/Tokenomics.jsx
@@ -2,8 +2,8 @@ import tokenomicsImg from "../../assets/images/Tokenomics.png";
 export default function Tokenomics() {
   return (
     <div className="relative">
-      <p className="relative z-0">Tokenomics</p>
-      <img className="absolute" src={tokenomicsImg} alt="" />
+      <p className="absolute z-10 top-20 text-3xl">Tokenomics</p>
+      <img className="mt-8" src={tokenomicsImg} alt="" />
     </div>
   );
 }
